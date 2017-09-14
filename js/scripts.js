522f9b9efe9eed27f1dd0deb89a8d2ec8abcc2d2
Kimberly Bordon and Susannah Lowe <susannah@>
2017-09-14 13:16:31 -0700

    Create tally function for form.

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,67 @@ $(document).ready(function() {
     var liberalTally = 0;
 
     var question1Answer = $("input:radio[name=question1]:checked").val();
+    var question2Answer = $("input:radio[name=question2]:checked").val();
+    var question3Answer = $("input:radio[name=question3]:checked").val();
+    var question4Answer = $("input:radio[name=question4]:checked").val();
+    var question5Answer = $("input:radio[name=question5]:checked").val();
+
 
     if (question1Answer === "conservative") {
-      alert(typeof conservativeTally);
       conservativeTally = conservativeTally + 1;
-      alert(conservativeTally);
     } else if (question1Answer === "moderate") {
-      alert(typeof moderateTally);
       moderateTally = moderateTally + 1;
-      alert(moderateTally);
     } else {
-      alert(typeof liberalTally);
       liberalTally = liberalTally + 1;
-      alert(liberalTally);
     }
+
+    if (question2Answer === "conservative") {
+      conservativeTally = conservativeTally + 1;
+    } else if (question2Answer === "moderate") {
+      moderateTally = moderateTally + 1;
+    } else {
+      liberalTally = liberalTally + 1;
+    }
+
+    if (question3Answer === "conservative") {
+      conservativeTally = conservativeTally + 1;
+    } else if (question3Answer === "moderate") {
+      moderateTally = moderateTally + 1;
+    } else {
+      liberalTally = liberalTally + 1;
+
+    }
+
+    if (question4Answer === "conservative") {
+      conservativeTally = conservativeTally + 1;
+    } else if (question4Answer === "moderate") {
+      moderateTally = moderateTally + 1;
+    } else {
+      liberalTally = liberalTally + 1;
+
+    }
+
+    if (question5Answer === "conservative") {
+      conservativeTally = conservativeTally + 1;
+    } else if (question5Answer === "moderate") {
+      moderateTally = moderateTally + 1;
+    } else {
+      liberalTally = liberalTally + 1;
+
+    }
+
+    if ( conservativeTally > moderateTally && conservativeTally > liberalTally) {
+      alert("conserative.");
+    } else if ( liberalTally > moderateTally && liberalTally > conservativeTally) {
+      alert("liberal.");
+    } else {
+      alert("moderate.");
+    }
+
+
+    alert(conservativeTally);
+    alert(moderateTally);
+    alert(liberalTally);
 
 
     event.preventDefault();
